1f6a033e06dd215be8982936781b40842712b006
Mickael Fernandez <micferna@z1r2p2.le-101.fr>
2019-07-21 23:04:50 +0200

    maj

--- C03/ex03/ft_strncat.c
+++ C03/ex03/ft_strncat.c
@@ -6,15 +6,39 @@
 /*   By: micferna <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/07/21 10:27:34 by micferna     #+#   ##    ##    #+#       */
-/*   Updated: 2019/07/21 13:38:56 by micferna    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/07/21 23:04:08 by micferna    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
+#include <stdio.h>
 char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
 	int	a;
+	int b;
 
 	a = 0;
+	b = 0;
 
+	while(dest[a] != '\0')
+		a++;
+	while (src[b] != '\0' && b < nb)
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+
+}
+
+int	main()
+{
+	char dest[30] = "abc";
+	char src[] = "deboiter";
+	
+	ft_strncat(dest, src, 15);
+	printf("%s\n", dest);
+	return (0);
 }
