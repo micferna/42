d272ed92dc2eba9d6c534975d8c7be3456e9db30
Mickael Fernandez <micferna@z1r1p1.le-101.fr>
2019-07-25 10:40:43 +0200

    push

---
+++ C06/ex02/ft_rev_params.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_rev_params.c                                  .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: micferna <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/07/24 17:19:47 by micferna     #+#   ##    ##    #+#       */
+/*   Updated: 2019/07/25 10:38:03 by micferna    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *src)
+{
+	int a;
+
+	a = 0;
+	while (src[a] != '\0')
+	{
+		ft_putchar(src[a]);
+		a++;
+	}
+	write(1, "\n", 1);
+}
+
+int		main(int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	while (argc > 1)
+	{
+		while (argv[argc -1][i] != '\0')
+		{
+			ft_putchar(argv[argc -1][i]);
+			i++;
+		}
+		ft_putchar('\n');
+		i = 0;
+		argc--;
+	}
+}
